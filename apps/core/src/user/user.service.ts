5f99d77f36ad6d1dd8916080e85db670e76e1629
zyhnbyyds <19939926438@163.com>
2024-09-27 09:59:55 +0800

    feat: add compodoc

--- apps/core/src/user/user.service.ts
+++ apps/core/src/user/user.service.ts
@@ -5,7 +5,6 @@ import { UpdateUserDto } from './dto/update-user.dto'
 @Injectable()
 export class UserService {
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user'
   }
 
   findAll() {
